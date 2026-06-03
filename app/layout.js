db0d58a44f6f46b219267b0be40054830915dca9
Devi Jetta <jettadevi289@gmail.com>
2026-06-03 17:54:41 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "LearnX Dashboard",
-  description: "Next - Gen Learning Dashboard",
+  description: "Next-Gen Learning Dashboard",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className="min-h-screen">
+        {children}
+      </body>
     </html>
   );
 }
